c9b1b868ef9bb92a56406a61a5f279866a1b8360
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-07-02 17:09:33 +0530

    Remmoved web

--- web/components/Layout.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import Styles from "../styles/Layout.module.scss";
-export default function Layout({ children }) {
-  return <div className={Styles.LayoutContainer}>{children}</div>;
-}
--- web/pages/user/dashboard.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function UserDashboard() {
-  return <div>UserDashboard</div>;
-}
