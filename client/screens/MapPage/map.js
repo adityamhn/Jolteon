f8b7a9fcc5b951ab820bfdb0978e125480cf8fb3
shero4 <goyal.dhruva@gmail.com>
2022-07-02 23:03:16 +0530

    done map

--- client/screens/MapPage/map.js
+++ client/screens/MapPage/map.js
@@ -5,13 +5,36 @@ import mapStyle from "./mapStyle";
 import * as Location from "expo-location";
 import { Fab, Icon } from "native-base";
 import { IconFill, IconOutline } from "@ant-design/icons-react-native";
+import { getAllSeller } from "../../services/auth.service";
+import { SwipeablePanel } from "rn-swipeable-panel";
 
 export function Map() {
   const [location, setLocation] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const [allmarkers, setAllmarkers] = useState(null);
+  const [panelProps, setPanelProps] = useState({
+    fullWidth: true,
+    openSmall: true,
+    showCloseButton: true,
+    onClose: () => closePanel(),
+    onPressCloseButton: () => closePanel(),
+  });
+  const [isPanelActive, setIsPanelActive] = useState(false);
+
+  const openPanel = () => {
+    setIsPanelActive(true);
+  };
+
+  const closePanel = () => {
+    setIsPanelActive(false);
+  };
 
   useEffect(() => {
     (async () => {
+      let data = await getAllSeller();
+      // console.log(data.sellerData);
+      setAllmarkers(data.sellerData);
+      // console.log(allmarkers);
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
         setErrorMsg("Permission to access location was denied");
@@ -33,17 +56,16 @@ export function Map() {
 
   return (
     <View style={styles.container}>
-      <Fab
-        renderInPortal={false}
-        shadow={2}
-        size="sm"
-        icon={<IconOutline name="plus" />}
-      />
-      {location && location.coords != null ? (
+      <SwipeablePanel {...panelProps} isActive={isPanelActive}>
+        <Text>Hi</Text>
+      </SwipeablePanel>
+      {location && location.coords && allmarkers != null ? (
         <MapView
           provider={PROVIDER_GOOGLE}
+          showsUserLocation={true}
+          showsMyLocationButton={true}
           customMapStyle={mapStyle}
-          region={{
+          initialRegion={{
             latitude: location.coords.latitude,
             longitude: location.coords.longitude,
             latitudeDelta: 0.0922,
@@ -60,18 +82,26 @@ export function Map() {
             title={"Your location"}
           ></Marker>
           {/* Other people station markers */}
-          <Marker
-            coordinate={{
-              latitude: location.coords.latitude + 0.001,
-              longitude: location.coords.longitude + 0.001,
-            }}
-            title={"Charging Station"}
-          >
-            <Image
-              source={require("./yellowmarker.png")}
-              style={{ height: 35, width: 35 }}
-            />
-          </Marker>
+          {allmarkers.map((marker, idx) => {
+            return (
+              <Marker
+                coordinate={{
+                  latitude: marker.latitude,
+                  longitude: marker.longitude,
+                }}
+                key={idx}
+                title={"Charging Station"}
+                onPress={() => {
+                  openPanel();
+                }}
+              >
+                <Image
+                  source={require("./yellowmarker.png")}
+                  style={{ height: 35, width: 35 }}
+                />
+              </Marker>
+            );
+          })}
           {/* Swap station markers */}
           <Marker
             coordinate={{
@@ -102,7 +132,7 @@ const styles = StyleSheet.create({
   },
   map: {
     width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    height: Dimensions.get("window").height - 50,
   },
 });
 
