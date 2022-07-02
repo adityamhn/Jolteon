bfa38e6aaa2a87c909f13b29345eca2b7ec8a7c5
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-07-02 12:57:30 +0530

    zustand removed for now

--- web/pages/index.js
+++ web/pages/index.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import { useStore } from "../store/store";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const { user, setUser } = useStore();
-
   return (
     <div className={styles.container}>
       <Head>
@@ -16,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to {user?.name} <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
@@ -53,16 +50,7 @@ export default function Home() {
             </p>
           </a>
         </div>
-        <div
-          onClick={() => {
-            setUser({
-              name: "Jolteon",
-              type: "Electric",
-            });
-          }}
-        >
-          Add some stuff
-        </div>
+        <div>Add some stuff</div>
       </main>
 
       <footer className={styles.footer}>
--- web/store/store.js
+++ web/store/store.js
@@ -1,7 +1,7 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
 
-export default useStore = create(
+export const useStore = create(
   persist((set) => ({
     user: {},
 
