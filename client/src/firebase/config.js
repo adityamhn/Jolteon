61a1efa11204bb3c972a31c87ca62ca43dfe8e4c
Aditya Mohan <aditya530026@gmail.com>
2022-07-02 12:45:49 +0530

    init firebase

--- client/src/firebase/config.js
+++ client/src/firebase/config.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase"
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,4 +14,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
