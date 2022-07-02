fd566e3f2b8deecf8e2380e1c428079d74d7b68d
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-07-02 17:00:08 +0530

    Butotn styles

--- new_web/components/Button.jsx
+++ new_web/components/Button.jsx
@@ -1,31 +1,34 @@
 import React from "react";
 import Styles from "/styles/Button.module.scss";
+import { Button as NButton } from "native-base";
+
 export default function Button({
   text,
   className,
-  disabled,
-  type,
-  id,
-  name,
-  value,
-  style,
   outlined,
+  onClick,
   ...props
 }) {
   return (
-    <button
-      className={`${Styles.MainButton} ${
-        outlined ? Styles.OutlinedButton : ""
-      } ${className}`}
-      disabled={disabled}
-      type={type}
-      id={id}
-      name={name}
-      value={value}
-      style={style}
+    // // <button
+    //   className={`${Styles.MainButton} ${
+    //     outlined ? Styles.OutlinedButton : ""
+    //   } ${className}`}
+    //   id={id}
+    //   name={name}
+    //   value={value}
+    //   style={style}
+    // {...props}
+    // >
+    // {text || props.children}
+    // </button>
+    <NButton
       {...props}
+      onPress={onClick}
+      bgColor={"#FFE040"}
+      _text={{ color: "#000", fontWeight: 600 }}
     >
       {text || props.children}
-    </button>
+    </NButton>
   );
 }
