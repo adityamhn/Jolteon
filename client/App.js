3c2bc3a3b6d7abd1585b4f7ce8cf2a014052179f
shero4 <goyal.dhruva@gmail.com>
2022-07-02 20:42:00 +0530

    works

--- client/App.js
+++ client/App.js
@@ -1,11 +1,10 @@
-import { Navigation } from './navigation';
-import "react-native-gesture-handler";
-import { SafeArea } from './components/SafeArea.component';
-import { NativeBaseProvider } from 'native-base';
+import React from "react";
+import firebase from "./firebase";
+import { Navigation } from "./navigation";
+// import "react-native-gesture-handler";
+import { SafeArea } from "./components/SafeArea.component";
+import { NativeBaseProvider } from "native-base";
 import { useFonts } from "expo-font";
-
-
-
 export default function App() {
   const [fontsLoaded] = useFonts({
     Montserrat: require("./fonts/Montserrat.ttf"),
@@ -22,4 +21,3 @@ export default function App() {
     </SafeArea>
   );
 }
-
--- client/navigation/index.js
+++ client/navigation/index.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { PreLoginNavigator } from "./prelogin.navigation";
+import app from "../firebase";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getFirestore, setDoc, doc, getDoc } from "firebase/firestore";
 import { AppNavigator } from "./app.navigation";
@@ -9,26 +10,25 @@ import { AppNavigator } from "./app.navigation";
 const auth = getAuth();
 const db = getFirestore();
 
-
 export const Navigation = () => {
-    const [currentUser, setUser] = useState(null);
+  const [currentUser, setUser] = useState(null);
 
-    useEffect(() => {
-        onAuthStateChanged(auth, async (user) => {
-            if (user) {
-                setUser(user);
-            } else {
-                setUser(null);
-            }
-        });
-    }, []);
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
 
-    return (
-        <>
-            <NavigationContainer>
-                {currentUser === null ? <PreLoginNavigator /> : <AppNavigator />}
-            </NavigationContainer>
-            <ExpoStatusBar style="auto" backgroundColor={"#0d0d0d"} />
-        </>
-    );
+  return (
+    <>
+      <NavigationContainer>
+        {currentUser === null ? <PreLoginNavigator /> : <AppNavigator />}
+      </NavigationContainer>
+      <ExpoStatusBar style="auto" backgroundColor={"#0d0d0d"} />
+    </>
+  );
 };
