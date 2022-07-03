e17102f21517b49e6c04a835409c4cfb13c5c2a2
S Sitaraman <sitaraman.raghav.india@gmail.com>
2022-07-03 10:56:01 +0530

    Finished web

---
+++ client/webpack.prod.js
@@ -0,0 +1,11 @@
+const merge = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "production",
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+});
